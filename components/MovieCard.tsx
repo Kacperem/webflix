9c3f00a07a67d5f9714276b57f564fb0db533c50
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-12-25 17:19:46 +0100

    InfoModal

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useRouter } from "next/router";
 
 import { BsFillPlayFill } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
+import useInfoModal from "@/hooks/useInfoModal";
 
 
 interface MovieCardProps {
@@ -11,6 +13,7 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+  const { openModal } = useInfoModal();
 
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -30,7 +33,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         w-full
         h-[12vw]"
       ></img>
-      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
+      <div onClick={() => openModal(data?.id)} className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
         <img
           src={data.thumbnailUrl}
           alt="Thumbnail"
@@ -53,6 +56,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={25} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+            <BiChevronDown size={30} class="text-white group-hover/item:text-neutral-300"/>
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
