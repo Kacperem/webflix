d679e1acbec7f3d80f5e572f0f9ed44333c75654
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-12-27 17:25:51 +0100

    Fix buttons

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -14,7 +14,7 @@ const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
       onClick={() => router.push(`/watch/${movieId}`)}
       className="bg-white rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-neutral-300 transition"
     >
-      <BsFillPlayFill size={30} className="mr-1" />
+      <BsFillPlayFill size={23} className="mr-1" />
       Play
     </div>
   );
